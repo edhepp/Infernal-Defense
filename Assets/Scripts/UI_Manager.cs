4bb36b3f882cbbb4a229bf20cc536687b24874ab
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2024-04-22 16:22:33 -0700

    removed unused Ui documents and combines to 1

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -15,16 +15,17 @@ public class UI_Manager : MonoBehaviour
     private UIDocument UIComponent = null;
     [SerializeField]
     private VisualTreeAsset MainMenu_Asset = null;
-    [SerializeField]
-    private VisualTreeAsset GamePlay_UI_Asset = null;
-    [SerializeField]
-    private VisualTreeAsset GamePlay_Menu_Asset = null;
-
-    private VisualElement MainMenuRoot = null;
-    private VisualElement GameplayMenuRoot = null;
-    private VisualElement UIGameplayRoot = null;
 
     //Quit, restart, score, lives, start, options, Continue
+    private Button _continue_B = null;
+    private Button _options_B = null;
+    private Button _start_B = null;
+    private Button _reset_B = null;
+    private Button _quit_B = null;
+
+    private Label _gameOver_L = null;
+    private Label _lives_L = null;
+    private Label _score_L = null;
 
     private bool _goodUIRefferences = true;
     void Start()
@@ -33,9 +34,9 @@ public class UI_Manager : MonoBehaviour
         // Run code if all refferences check out as good
         if (_goodUIRefferences)
         {
-            SetUIRoots();
-            //Main menue setup
-            //UIComponent.rootVisualElement.Add(MainMenuRoot);
+            UIComponent.visualTreeAsset = MainMenu_Asset;
+            //Main menu setup
+            SetUIElements();
         }
         else return;
     }
@@ -52,37 +53,48 @@ public class UI_Manager : MonoBehaviour
         }
         if(MainMenu_Asset == null)
         {
-            MainMenu_Asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Prefabs/Managers/UI/Main_Menu.uxml");
+            MainMenu_Asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Prefabs/Managers/UI/MainMenu_UI.uxml");
             if(MainMenu_Asset)
-                Debug.Log("AssetTree MainMenu not found: set threw code");
+                Debug.Log("AssetTree MainMenu: set threw code");
             else
-                Debug.Log("AssetTree MainMenu not found Assets/Prefabs/Managers/UI/Main_Menu.uxml");
+                Debug.Log("AssetTree MainMenu not found Assets/Prefabs/Managers/UI/MainMenu_UI.uxml");
         }
-        if(GamePlay_UI_Asset == null)
-        {
-            GamePlay_Menu_Asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Prefabs/Managers/UI/GamePlay_Menu.uxml");
-            if (GamePlay_UI_Asset)
-                Debug.Log("AssetTree Gameplay_UI_root gameplay not found: set threw code");
-            else
-                Debug.Log("AssetTree Gameplay_UI not found Assets/Prefabs/Managers/UI/GamePlay_Menu.uxml");
-        }
-        if(GamePlay_UI_Asset == null)
-        {
-            GamePlay_UI_Asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Prefabs/Managers/UI/GamePlay_UI.uxml");
-            if (GamePlay_UI_Asset)
-                Debug.Log("AssetTree GamePlay_UI not founs: set threw code");
-            else
-                Debug.Log("AssetTree Gameplay_UI not found Assets/Prefabs/Managers/UI/GamePlay_UI.uxml");
-        }
-        _goodUIRefferences = UIComponent & MainMenu_Asset & GamePlay_Menu_Asset & GamePlay_UI_Asset;
+        _goodUIRefferences = UIComponent & MainMenu_Asset;
     }
-    private void SetUIRoots()
+    private VisualElement root = null;
+    private void SetUIElements()
     {
-        MainMenuRoot = MainMenu_Asset.CloneTree();
-        UIGameplayRoot = GamePlay_UI_Asset.CloneTree();
-        GameplayMenuRoot = GamePlay_Menu_Asset.CloneTree();
+        root = UIComponent.rootVisualElement;
+
+        _continue_B = root.Q<Button>("ContinueButton");
+            Debug.Log(_continue_B != null ? "_continue_B Set" : "_continue Not found");
+        _options_B = root.Q<Button>("OptionsButton");
+            Debug.Log(_options_B != null ? "_options_B Set" : "_options_B Not found");
+        _start_B = root.Q<Button>("StartButton");
+            Debug.Log(_start_B != null ? "_start_B Set" : "_start_B Not found");
+        _reset_B = root.Q<Button>("ResetButton");
+            Debug.Log(_reset_B != null ? "_reset_B Set" : "_reset_B Not found");
+        _quit_B = root.Q<Button>("QuitButton");
+            Debug.Log(_quit_B != null ? "_quit_B Set" : "_quit_B Not found");
+
+        _gameOver_L = root.Q<Label>("GameOverLabel");
+            Debug.Log(_gameOver_L != null ? "_gameOver_L Set" : "_gameOver_L Not found");
+        _lives_L = root.Q<Label>("ScoreLabel");
+            Debug.Log(_lives_L != null ? "_lives_L Set" : "_lives_L Not found");
+        _score_L = root.Q<Label>("LivesLabel");
+            Debug.Log(_score_L != null ? "_score_L Set" : "_score_L Not found");
+
+        
     }
-    private void RemoveAndReplaceVisualTree(VisualTreeAsset replaceWith)
+    private void MainMenu()
+    {
+        // Start and Quit Changes
+    }
+    private void GamePlayMenu()
+    {
+
+    }
+    private void GameplayUI()
     {
 
     }
@@ -90,15 +102,5 @@ public class UI_Manager : MonoBehaviour
     {
         // remove Gameplay UI
         // Add a Game over Screen
-    }
-    private void GameStarted()
-    {
-        // add Gameplay UI when game is started 
-        // remove menu
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
