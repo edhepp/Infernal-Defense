fd3b7283d7adcc6607f2c4b24bb82b7f556d937d
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2024-04-22 23:26:42 -0700

    Simple UI complete
    1. Start button with events
    2. Continue button with events
    3. Continue button with events
    4. Options button with events
    5. Restart button with events
    6. Quit Button with events
    7. Lives, Score labels must be exposed to game manager
    7. Added GameOver labels.

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -11,6 +11,21 @@ using static Unity.Collections.AllocatorManager;
 
 public class UI_Manager : MonoBehaviour
 {
+    //Todo: Create a singleton and dont destroy on load
+    //Todo: do something with Options like volume control (Music, Sound FX)
+    public delegate void MenuButtons();
+    public static event MenuButtons ContinueEvent;
+    public static event MenuButtons OptionsEvent;
+    public static event MenuButtons StartEvent;
+    public static event MenuButtons RestartEvent;
+    public static event MenuButtons QuitEvent;
+
+    public delegate void GameState();
+    public static event GameState GameOverState; // move to game manager
+    public static event GameState PausedState; // move to input manager
+
+    //Todo: access labels (score and lives) threw singalton instance
+
     [SerializeField]
     private UIDocument UIComponent = null;
     [SerializeField]
@@ -20,7 +35,7 @@ public class UI_Manager : MonoBehaviour
     private Button _continue_B = null;
     private Button _options_B = null;
     private Button _start_B = null;
-    private Button _reset_B = null;
+    private Button _restart_B = null;
     private Button _quit_B = null;
 
     private Label _gameOver_L = null;
@@ -30,6 +45,8 @@ public class UI_Manager : MonoBehaviour
     private bool _goodUIRefferences = true;
     void Start()
     {
+        //todo: Listen for event from Input manager esc for Pausing game
+        //todo: Listen for event from Game manager for Game over
         VerifyUIRef();
         // Run code if all refferences check out as good
         if (_goodUIRefferences)
@@ -37,8 +54,26 @@ public class UI_Manager : MonoBehaviour
             UIComponent.visualTreeAsset = MainMenu_Asset;
             //Main menu setup
             SetUIElements();
+            MainMenu();
+            RegisterAllCallbacks();
         }
         else return;
+    }
+    private void RegisterAllCallbacks()
+    {
+        _continue_B.RegisterCallback<ClickEvent>(_ => ContinueButton());
+        _restart_B.RegisterCallback<ClickEvent>(_ => RestartButton());
+        _options_B.RegisterCallback<ClickEvent>(_ => OptionsEvent?.Invoke());
+        _start_B.RegisterCallback<ClickEvent>(_ => StartButton());
+        _quit_B.RegisterCallback<ClickEvent>(_ => QuitButton());
+    }
+    private void UnRegisterAllCallbacks()
+    {
+        _continue_B.UnregisterCallback<ClickEvent>(_ => ContinueEvent?.Invoke());
+        _restart_B.UnregisterCallback<ClickEvent>(_ => RestartEvent?.Invoke());
+        _options_B.UnregisterCallback<ClickEvent>(_ => OptionsEvent?.Invoke());
+        _start_B.UnregisterCallback<ClickEvent>(_ => StartEvent?.Invoke());
+        _quit_B.UnregisterCallback<ClickEvent>(_ => QuitEvent?.Invoke());
     }
     private void VerifyUIRef()
     {
@@ -59,7 +94,7 @@ public class UI_Manager : MonoBehaviour
             else
                 Debug.Log("AssetTree MainMenu not found Assets/Prefabs/Managers/UI/MainMenu_UI.uxml");
         }
-        _goodUIRefferences = UIComponent & MainMenu_Asset;
+        _goodUIRefferences = UIComponent && MainMenu_Asset;
     }
     private VisualElement root = null;
     private void SetUIElements()
@@ -72,8 +107,8 @@ public class UI_Manager : MonoBehaviour
             Debug.Log(_options_B != null ? "_options_B Set" : "_options_B Not found");
         _start_B = root.Q<Button>("StartButton");
             Debug.Log(_start_B != null ? "_start_B Set" : "_start_B Not found");
-        _reset_B = root.Q<Button>("ResetButton");
-            Debug.Log(_reset_B != null ? "_reset_B Set" : "_reset_B Not found");
+        _restart_B = root.Q<Button>("RestartButton");
+            Debug.Log(_restart_B != null ? "_restart_B Set" : "_reset_B Not found");
         _quit_B = root.Q<Button>("QuitButton");
             Debug.Log(_quit_B != null ? "_quit_B Set" : "_quit_B Not found");
 
@@ -83,24 +118,80 @@ public class UI_Manager : MonoBehaviour
             Debug.Log(_lives_L != null ? "_lives_L Set" : "_lives_L Not found");
         _score_L = root.Q<Label>("LivesLabel");
             Debug.Log(_score_L != null ? "_score_L Set" : "_score_L Not found");
-
-        
+    }
+    private void ContinueButton()
+    {
+        ContinueEvent?.Invoke();
+        GameplayUI();
+    }
+    private void RestartButton()
+    {
+        RestartEvent?.Invoke();
+        GameplayUI();
+    }
+    private void StartButton()
+    {
+        StartEvent?.Invoke();
+        GameplayUI();
+    }
+    private void QuitButton()
+    {
+        QuitEvent?.Invoke();
+        MainMenu();
     }
     private void MainMenu()
     {
-        // Start and Quit Changes
+        // Continue false, Options true, Start true, Quit true, restart false
+        _start_B.style.display = DisplayStyle.Flex;
+        _options_B.style.display = DisplayStyle.Flex;
+        _quit_B.style.display = DisplayStyle.Flex;
+
+        _continue_B.style.display = DisplayStyle.None;
+        _restart_B.style.display = DisplayStyle.None;
+        _gameOver_L.style.display = DisplayStyle.None;
+        _lives_L.style.display = DisplayStyle.None;
+        _score_L.style.display = DisplayStyle.None;
+        // GameOver false, Lives false, score false.
     }
     private void GamePlayMenu()
     {
+        _continue_B.style.display = DisplayStyle.Flex;
+        _options_B.style.display = DisplayStyle.Flex;
+        _quit_B.style.display = DisplayStyle.Flex;
 
+        //_lives_L.style.display = DisplayStyle.None;
+        //_score_L.style.display = DisplayStyle.None;
+
+        _start_B.style.display = DisplayStyle.None;
+        _restart_B.style.display = DisplayStyle.None;
+        _gameOver_L.style.display = DisplayStyle.None;
     }
     private void GameplayUI()
     {
+        _lives_L.style.display = DisplayStyle.Flex;
+        _score_L.style.display = DisplayStyle.Flex;
 
+        _start_B.style.display = DisplayStyle.None;
+        _options_B.style.display = DisplayStyle.None;
+        _quit_B.style.display = DisplayStyle.None;
+        _continue_B.style.display = DisplayStyle.None;
+        _restart_B.style.display = DisplayStyle.None;
+        _gameOver_L.style.display = DisplayStyle.None;
     }
     private void GameOver()
     {
-        // remove Gameplay UI
-        // Add a Game over Screen
+        _restart_B.style.display = DisplayStyle.Flex;
+        _quit_B.style.display = DisplayStyle.Flex;
+        _gameOver_L.style.display = DisplayStyle.Flex;
+
+        _start_B.style.display = DisplayStyle.None;
+        _options_B.style.display = DisplayStyle.None;
+        _continue_B.style.display = DisplayStyle.None;
+        _lives_L.style.display = DisplayStyle.None;
+        _score_L.style.display = DisplayStyle.None;
+    }
+    private void OnDestroy()
+    {
+        UnRegisterAllCallbacks();
     }
 }
