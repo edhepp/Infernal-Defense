c1a7fd8a2bd42108e5ee8cfc4b2a2e3c8aa716c4
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2024-05-13 17:49:57 -0700

    organizng

---
+++ Assets/Scripts/Behaviours/HutBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HutBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private int _lives = 3;
+    [SerializeField]
+    private bool _isOnFire = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Listen for input events for events
+        //
+    }
+    private void RepairHut()
+    {
+
+    }
+    private void DamageHut()
+    {
+
+    }
+    private void IsOnFire()
+    {
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Check if hut ran out of lives
+    }
+}
